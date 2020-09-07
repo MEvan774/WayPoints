0de2306ffbd6f2697c81b767389218d6d3373d87
MEvan774 <57196554+MEvan774@users.noreply.github.com>
2020-09-07 13:08:28 +0200

    Add files via upload
    Je kan objecten in de array doen om zo waypoints te creëren.
    als het object bij de laatste waypoint is gaat hij automatisch weer naar de eerste waypoint

---
+++ Cube.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+    public GameObject[] wayPoints;
+    public int currentPoint;
+    private int endPoint;
+
+    public float minDistance;
+    public float speed;
+
+    private void Start()
+    {
+        endPoint = wayPoints.Length;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (currentPoint == endPoint)
+        {
+            ResetArray();
+        }
+
+            float distance = Vector3.Distance(gameObject.transform.position, wayPoints[currentPoint].transform.position);
+        if (distance > minDistance)
+        {
+            gameObject.transform.LookAt(wayPoints[currentPoint].transform.position);
+            gameObject.transform.position += gameObject.transform.forward * speed * Time.deltaTime;
+        }
+        else
+        {
+            Debug.Log(wayPoints[currentPoint]);
+            currentPoint++;
+        }
+    }
+
+    void ResetArray()
+    {
+        currentPoint = 0;;
+    }
+}
