9935efacb7dcd28b2fde9d2b00356a313b0125c2
MEvan774 <57196554+MEvan774@users.noreply.github.com>
2020-09-28 12:30:48 +0200

    Add files via upload
    28/09/2020
    
    GameEventListener script werkt via scriptable objects, hiermee wordt het makkelijker om events te maken.
    
    als de enemy op de laatste waypoint komt word een event geactiveerd. De event zorgt ervoor dat de speler levens verliest en dat de camera schud.

---
+++ EnemyPath.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class EnemyPath : MonoBehaviour
+{
+    public WayPointLine[] wayPointArray;
+
+    public Transform[] wayPoints;
+
+
+    public int currentPoint;
+    private int endPoint;
+    private bool atDestination;
+
+    public float minDistance;
+    public float speed;
+
+    public UnityEvent attackEvent;
+
+    private void Start()
+    {
+        endPoint = wayPoints.Length;
+        //wayPoints = wayPointArray;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        if (currentPoint == endPoint)
+        {
+            ResetArray();
+            AttackEvent();
+        }
+        if (!atDestination)
+        {
+            float distance = Vector3.Distance(gameObject.transform.position, wayPoints[currentPoint].transform.position);
+            if (distance > minDistance)
+            {
+                gameObject.transform.LookAt(wayPoints[currentPoint].transform.position);
+                gameObject.transform.position += gameObject.transform.forward * speed * Time.deltaTime;
+            }
+            else
+            {
+                Debug.Log(wayPoints[currentPoint]);
+                currentPoint++;
+            }
+        }
+
+    }
+
+    void ResetArray()
+    {
+        currentPoint = 0;;
+    }
+
+    void AttackEvent()
+    {
+        attackEvent.Invoke();
+        Debug.LogWarning("FinalPoint Bereikt", wayPoints[currentPoint]);
+    }
+
+}
